5e7bb19ae1aae930c1a53a4c23790007625b0ff1
fahadtufail007 <fahadtufail873@gmail.com>
2026-05-07 00:52:37 +0500

    adds account routes

---
+++ src/routes/accountRoutes.jsx
@@ -0,0 +1,30 @@
+import { Route } from 'react-router-dom';
+import ProtectedRoute from '../components/shared/ProtectedRoute';
+import Layout from '../components/layout/Layout';
+import DashboardPage from '../pages/DashboardPage';
+import ProfilePage from '../pages/account/ProfilePage';
+import ChangePasswordPage from '../pages/account/ChangePasswordPage';
+import InsuranceFormMockupPage from '../pages/demo/InsuranceFormMockupPage';
+
+const anyRole = (children) => (
+  <ProtectedRoute requireAllRoles={true}>
+    <Layout>{children}</Layout>
+  </ProtectedRoute>
+);
+
+const accountRoutes = [
+  <Route key="/dashboard" path="/dashboard" element={anyRole(<DashboardPage />)} />,
+  <Route key="/profile" path="/profile" element={anyRole(<ProfilePage />)} />,
+  <Route key="/change-password" path="/change-password" element={anyRole(<ChangePasswordPage />)} />,
+  <Route
+    key="/demo/insurance-form"
+    path="/demo/insurance-form"
+    element={
+      <ProtectedRoute requireAllRoles={true}>
+        <InsuranceFormMockupPage />
+      </ProtectedRoute>
+    }
+  />,
+];
+
+export default accountRoutes;
