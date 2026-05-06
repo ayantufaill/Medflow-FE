48c603c6b29695d58102969eb9f139feeb4a495b
fahadtufail007 <fahadtufail873@gmail.com>
2026-05-07 00:51:58 +0500

    adds theme

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: '"Manrope", "Segoe UI", sans-serif',
+  },
+});
+
+export default theme;
